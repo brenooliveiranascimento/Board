893cb739036e0b5145e27e05534c0818a2ae2302
BrenoNascimento <breno564nascimento@gmail.com>
2022-02-22 11:52:28 -0300

    novo

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -4,6 +4,7 @@ import { Provider as SessionProvider }  from 'next-auth/client'
 import { PayPalScriptProvider } from '@paypal/react-paypal-js'
 import { Header } from '../Components/Header';
 //Produção: AXKyBzUdPZyKzY0L7JZlqCVKcQTM0UfQa5jcOIbNKNwMtVbf8lstH0ZZasZKj2NSRazGbe84B3nURcPo
+//teste: AUgWmEfZVwpBxGRlPFacIWUprVpEp-_hNmSSTjJMfGoe3-9IaJRnRDoMKpnF6mqGW0XpJ8h4K4HzzxVr
 function MyApp({ Component, pageProps }: AppProps) {
 
   const initialOpations = {
